a8e761243cc330965c9a3acd525343af8d3c1493
Lucas Badiale <lcbadial@gmail.com>
2025-09-17 10:12:42 -0300

    Fix error when READ_CONTACTS permission was not granted

--- app/src/main/java/dev/badiale/callblocker/CallBlockerScreeningService.kt
+++ app/src/main/java/dev/badiale/callblocker/CallBlockerScreeningService.kt
@@ -1,6 +1,8 @@
 package dev.badiale.callblocker;
 
+import android.Manifest
 import android.content.ContentResolver
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.provider.CallLog.Calls.BLOCKED_TYPE
@@ -8,6 +10,7 @@ import android.provider.ContactsContract
 import android.telecom.Call
 import android.telecom.CallScreeningService
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.core.database.getStringOrNull
 import androidx.core.net.toUri
 import dev.badiale.callblocker.domain.repository.CallLogRepository
@@ -68,6 +71,13 @@ class CallBlockerScreeningService : CallScreeningService() {
     }
 
     private fun findContactOrNull(number: String): Contact? {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_CONTACTS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return null;
+        }
         val resolver: ContentResolver = contentResolver
         val uri: Uri = ContactsContract.PhoneLookup.CONTENT_FILTER_URI.buildUpon()
             .appendPath(number)
